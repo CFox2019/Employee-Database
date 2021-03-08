e84f9b301e50b8cc90dce6ed26dc4445c8dc6b3f
Courtney Fox <courtney.fox@me.com>
2021-03-08 14:28:11 -0500

    created start function that prompts initial questions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const cTable = require('console.table');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -9,5 +10,37 @@ const connection = mysql.createConnection({
     user: 'root',
 
     password: 'tL!TYgh4VHMP',
-    database: 'employees_DB',
+    database: 'department_DB',
+});
+
+const start = () => {
+    inquirer
+        .prompt({
+            name:'addViewOrUpdate',
+            type: 'list',
+            message: 'Would you like to add, view, or update?',
+            choices: [
+                'Add departments, roles, employees',
+                'View departments, roles, employees',
+                'Update employee roles',
+                'EXIT'
+            ],
+        })
+        .then((answer) => {
+            if (answer.addViewOrUpdate === 'Add departments, roles, employees') {
+                addInfo();
+            } else if (answer.addViewOrUpdate === 'View departments, roles, employees') {
+                viewInfo();
+            } else if (answer.addViewOrUpdate === 'Update employee roles') {
+                updateInfo();
+            } else {
+                connection.end();
+            }
+        });
+};
+
+connection.connect((err) => {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
 });
