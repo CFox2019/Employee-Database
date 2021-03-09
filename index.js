08a769bd624a793f8394bd5e1ce04e6bae451835
Courtney Fox <courtney.fox@me.com>
2021-03-09 13:23:08 -0500

    added SELECT functions that show results  to 'view' functions

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const connection = mysql.createConnection({
 const start = () => {
     inquirer
         .prompt({
-            name:'addViewOrUpdate',
+            name: 'addViewOrUpdate',
             type: 'list',
             message: 'Would you like to add, view, or update information?',
             choices: [
@@ -42,22 +42,22 @@ const start = () => {
 const addInfo = () => {
     inquirer
         .prompt({
-            name:'addDeptRoleEmployee',
+            name: 'addDeptRoleEmployee',
             type: 'list',
             message: 'What information would you like to add?',
             choices: [
-                'Departments',
-                'Roles',
-                'Employees',
+                'Department',
+                'Role',
+                'Employee',
                 'EXIT'
             ],
         })
         .then((answer) => {
-            if (answer.addDeptRoleEmployee === 'Departments') {
+            if (answer.addDeptRoleEmployee === 'Department') {
                 addDept();
-            } else if (answer.addDeptRoleEmployee === 'Roles') {
+            } else if (answer.addDeptRoleEmployee === 'Role') {
                 addRole();
-            } else if (answer.addDeptRoleEmployee === 'Employees') {
+            } else if (answer.addDeptRoleEmployee === 'Employee') {
                 addEmployee();
             } else {
                 connection.end();
@@ -70,32 +70,25 @@ const addDept = () => {
     inquirer
         .prompt({
             name: 'addDept',
-            type: 'input',
-            message: 'What department would you like to add?'
+            type: 'list',
+            message: 'What department would you like to add?',
+            choices: [
+                'Sales',
+                'Engineering',
+                'Finance',
+                'Legal'
+            ]
         })
         .then((answer) => {
             // when finished prompting, insert the department name into the db with the text input
             connection.query(
                 'INSERT INTO departments SET ?',
                 {
-
+                    department_name: answer.addDept,
                 },
                 (err) => {
                     if (err) throw err;
                     console.log('Your department was added successfully!')
-                    // prompt the user for if they want to add another department or exit
-                    .then({
-                        name: 'addDeptOrExit',
-                        type: 'confirm',
-                        message: 'Would you like to add another department?'
-                    })
-                    .then((answer) => {
-                        if (answer.addDeptOrExit === 'yes') {
-                            addDept();
-                        } else {
-                            connection.end();
-                        }
-                    });
                 }
             )
         })
@@ -104,77 +97,76 @@ const addDept = () => {
 // **** function for adding roles
 const addRole = () => {
     inquirer
-        .prompt({
-            name: 'addRole',
-            type: 'input',
-            message: 'What role would you like to add?'
-        })
+        .prompt([
+            {
+                name: 'addRoleTitle',
+                type: 'list',
+                message: 'What role would you like to add?',
+                choices: [
+                    'Sales Lead',
+                    'Salesperson',
+                    'Lead Engineer',
+                    'Software Engineer',
+                    'Accountant',
+                    'Legal Team Lead',
+                    'Lawyer'
+                ],
+            },
+            {
+                name: 'addSalary',
+                type: 'list',
+                message: 'What is the salary of this role?',
+                choices: [
+                    '80000',
+                    '100000',
+                    '120000',
+                    '150000',
+                    '190000',
+                    '250000'
+                ],
+            },
+        ])
         .then((answer) => {
             // when finished prompting, insert the role name into the db with the text input
             connection.query(
                 'INSERT INTO roles SET ?',
                 {
-
+                    title: answer.addRoleTitle,
+                    salary: answer.addSalary,
                 },
                 (err) => {
                     if (err) throw err;
                     console.log('Your role was added successfully!')
-                    // prompt the user for if they want to add another department or exit
-                    .then({
-                        name: 'addRoleOrExit',
-                        type: 'confirm',
-                        message: 'Would you like to add another role?'
-                    })
-                    .then((answer) => {
-                        if (answer.addRoleOrExit === 'yes') {
-                            addRole();
-                        } else {
-                            connection.end();
-                        }
-                    });
-                }
-            )
+                })
         })
 };
 
 // **** function for adding employees
 const addEmployee = () => {
     inquirer
-        .prompt(
+        .prompt([
             {
-                name: 'addEmployeesFirstName',
+                name: 'addFirstName',
                 type: 'input',
                 message: 'What is the employees first name?'
             },
             {
-                name: 'addEmployeesLastName',
+                name: 'addLastName',
                 type: 'input',
                 message: 'What is the employees last name?'
             }
-        )
+        ])
         .then((answer) => {
             // when finished prompting, insert the employee name into the db with the text input
             connection.query(
                 'INSERT INTO employees SET ?',
                 {
-
+                    first_name: answer.addFirstName,
+                    last_name: answer.addLastName,
                 },
                 (err) => {
                     if (err) throw err;
                     console.log('Your employee was added successfully!')
-                    // prompt the user for if they want to add another department or exit
-                    .then({
-                        name: 'addEmployeeOrExit',
-                        type: 'confirm',
-                        message: 'Would you like to add another employee?'
-                    })
-                    .then((answer) => {
-                        if (answer.addEmployeeOrExit === 'yes') {
-                            addEmployee();
-                        } else {
-                            connection.end();
-                        }
-                    });
                 }
             )
         })
@@ -186,7 +178,7 @@ const addEmployee = () => {
 const viewInfo = () => {
     inquirer
         .prompt({
-            name:'viewDeptRoleEmployee',
+            name: 'viewDeptRoleEmployee',
             type: 'list',
             message: 'What information would you like to view?',
             choices: [
@@ -211,48 +203,58 @@ const viewInfo = () => {
 
 // *** viewDepts function should display all departments that have been entered
 const viewDept = () => {
-
+    connection.query("SELECT * FROM departments", (err, result) => {
+        if (err) throw err;
+        console.log(result);
+    });
 };
 
 // *** viewRoles function should display all roles
 const viewRole = () => {
-
+    connection.query("SELECT * FROM roles", (err, result) => {
+        if (err) throw err;
+        console.log(result);
+    });
 };
 
 // *** viewEmployees function should display all employees with their info
 const viewEmployee = () => {
-
+    connection.query("SELECT * FROM employees", function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
 };
 
 
-// Updating employee roles
+    // Updating employee roles
 
-// function to handle updating employees
-const updateInfo = () => {
-    inquirer
-        .prompt({
-            name: 'updateEmployee',
-            type: 'confirm',
-            message: 'Is the employee a manager?'
-        })
-        .then((answer)=> {
-            if (answer.updateEmployee === 'yes') {
-                updateManager();
-            } else if (answer.updateEmployee === 'no') {
-                updateEmployeeRole();
-            }
-        });
-};
+    // function to handle updating employees
+    const updateInfo = () => {
+        inquirer
+            .prompt({
+                name: 'updateEmployee',
+                type: 'confirm',
+                message: 'Is the employee a manager?'
+            })
+            .then((answer) => {
+                if (answer.updateEmployee === 'yes') {
+                    updateManager();
+                } else if (answer.updateEmployee === 'no') {
+                    updateEmployeeRole();
+                }
+            });
+    }
+
 
 // *** updateManager function allows the employee managers to be updated
 const updateManager = () => {
 
-}
+};
 
 // *** updateEmployees function allows the employees role to be entered
 const updateEmployeeRole = () => {
 
-}
+};
 
 connection.connect((err) => {
     if (err) throw err;
